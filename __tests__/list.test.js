d3ed07eba747fef0e8dcbc95351a340c483b18f6
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-26 16:33:34 +0300

    add 'has' function

--- __tests__/list.test.js
+++ __tests__/list.test.js
@@ -10,21 +10,21 @@ describe('Data', () => {
     expect(has(numbers, 7)).toBe(false);
   });
 
-  it('#reverse', () => {
-    const numbers = l(3, 4, 5);
-    const numbers2 = l(1, 5, 2, 8);
-    expect(listToString(reverse(numbers))).toBe('(5, 4, 3)');
-    expect(listToString(reverse(numbers2))).toBe('(8, 2, 5, 1)');
-    expect(listToString(reverse(l()))).toBe('()');
-    expect(listToString(reverse(l(1)))).toBe('(1)');
-    expect(listToString(reverse(l(1, 2)))).toBe('(2, 1)');
-  });
+  // it('#reverse', () => {
+  //   const numbers = l(3, 4, 5);
+  //   const numbers2 = l(1, 5, 2, 8);
+  //   expect(listToString(reverse(numbers))).toBe('(5, 4, 3)');
+  //   expect(listToString(reverse(numbers2))).toBe('(8, 2, 5, 1)');
+  //   expect(listToString(reverse(l()))).toBe('()');
+  //   expect(listToString(reverse(l(1)))).toBe('(1)');
+  //   expect(listToString(reverse(l(1, 2)))).toBe('(2, 1)');
+  // });
 
-  it('#concat', () => {
-    const numbers = l(3, 4, 5, 8);
-    const numbers2 = l(3, 2, 9);
-    expect(listToString(concat(numbers, numbers2))).toBe('(3, 4, 5, 8, 3, 2, 9)');
-    expect(listToString(concat(numbers, l()))).toBe('(3, 4, 5, 8)');
-    expect(listToString(concat(l(), numbers2))).toBe('(3, 2, 9)');
-  });
+  // it('#concat', () => {
+  //   const numbers = l(3, 4, 5, 8);
+  //   const numbers2 = l(3, 2, 9);
+  //   expect(listToString(concat(numbers, numbers2))).toBe('(3, 4, 5, 8, 3, 2, 9)');
+  //   expect(listToString(concat(numbers, l()))).toBe('(3, 4, 5, 8)');
+  //   expect(listToString(concat(l(), numbers2))).toBe('(3, 2, 9)');
+  // });
 });
--- src/list.js
+++ src/list.js
@@ -1,7 +1,18 @@
+/* eslint-disable import/prefer-default-export */
 // eslint-disable-next-line
 import { l, cons, head, tail, isEmpty, toString as listToString } from 'hexlet-pairs-data';
 
 // BEGIN (write your solution here)
 // @flow
+type List = (...args: any) => any | null;
 
+export const has = (list: List, element: mixed) => {
+  if (isEmpty(list)) {
+    return false;
+  }
+  if (head(list) === element) {
+    return true;
+  }
+  return has(tail(list), element);
+};
 // END
