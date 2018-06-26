af4c9296f86fc5d2dd6064546dca083fc7761f55
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-26 16:50:16 +0300

    add 'reverse' function

--- __tests__/list.test.js
+++ __tests__/list.test.js
@@ -2,13 +2,13 @@ import { l, toString as listToString } from 'hexlet-pairs-data'; // eslint-disab
 import { has, reverse, concat } from '../src/list';
 
 describe('Data', () => {
-  it('#has', () => {
-    const numbers = l(3, 4, 5, 8);
-    expect(has(numbers, 3)).toBe(true);
-    expect(has(numbers, 8)).toBe(true);
-    expect(has(numbers, 0)).toBe(false);
-    expect(has(numbers, 7)).toBe(false);
-  });
+  // it('#has', () => {
+  //   const numbers = l(3, 4, 5, 8);
+  //   expect(has(numbers, 3)).toBe(true);
+  //   expect(has(numbers, 8)).toBe(true);
+  //   expect(has(numbers, 0)).toBe(false);
+  //   expect(has(numbers, 7)).toBe(false);
+  // });
 
   // it('#reverse', () => {
   //   const numbers = l(3, 4, 5);
@@ -20,11 +20,11 @@ describe('Data', () => {
   //   expect(listToString(reverse(l(1, 2)))).toBe('(2, 1)');
   // });
 
-  // it('#concat', () => {
-  //   const numbers = l(3, 4, 5, 8);
-  //   const numbers2 = l(3, 2, 9);
-  //   expect(listToString(concat(numbers, numbers2))).toBe('(3, 4, 5, 8, 3, 2, 9)');
-  //   expect(listToString(concat(numbers, l()))).toBe('(3, 4, 5, 8)');
-  //   expect(listToString(concat(l(), numbers2))).toBe('(3, 2, 9)');
-  // });
+  it('#concat', () => {
+    const numbers = l(3, 4, 5, 8);
+    const numbers2 = l(3, 2, 9);
+    expect(listToString(concat(numbers, numbers2))).toBe('(3, 4, 5, 8, 3, 2, 9)');
+    expect(listToString(concat(numbers, l()))).toBe('(3, 4, 5, 8)');
+    expect(listToString(concat(l(), numbers2))).toBe('(3, 2, 9)');
+  });
 });
--- src/list.js
+++ src/list.js
@@ -6,7 +6,7 @@ import { l, cons, head, tail, isEmpty, toString as listToString } from 'hexlet-p
 // @flow
 type List = (...args: any) => any | null;
 
-export const has = (list: List, element: mixed) => {
+export const has = (list: List, element: mixed): boolean => {
   if (isEmpty(list)) {
     return false;
   }
@@ -14,5 +14,23 @@ export const has = (list: List, element: mixed) => {
     return true;
   }
   return has(tail(list), element);
+};
+
+export const reverse = (list: List): List => {
+  const iter = (acc: List, udList: List) => {
+    if (isEmpty(udList)) {
+      return acc;
+    }
+    return iter(cons(head(udList), acc), tail(udList));
+  };
+  return iter(l(), list);
+};
+
+export const concat = (list1: List, list2: List): List => {
+  if (isEmpty(list1)) {
+    return list2;
+  }
+
+  return cons(head(list1), concat(tail(list1), list2));
 };
 // END
