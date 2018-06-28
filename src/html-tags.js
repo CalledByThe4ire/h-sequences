966e9a132493d91e2877a97529fc25a5e9447cc9
Andrey Karamnoff <karamnoff@gmail.com>
2018-06-28 15:41:59 +0300

    add hexlet' solution

--- src/html-tags.js
+++ src/html-tags.js
@@ -7,5 +7,26 @@ import { l, isEmpty, head, tail, cons as consList, toString as listToString } fr
 export const make = () => l();
 // BEGIN (write your solution here)
 // @flow
+type Node = {
+  name: mixed,
+  value: mixed
+};
+type List = (...args: mixed) => mixed | null;
 
+export const append = (dom: List, element: Node) => consList(element, dom);
+
+export const node = (tag: mixed, content: mixed): Node => cons(tag, content);
+
+export const name = (element: Node): mixed => car(element);
+export const value = (element: Node): mixed => cdr(element);
+
+export const toString = (elements: List): string => {
+  if (isEmpty(elements)) {
+    return '';
+  }
+  const element = head(elements);
+  const tag = name(element);
+  return `${toString(tail(elements))}<${tag}>${value(element)}</${tag}>`;
+};
 // END
+
