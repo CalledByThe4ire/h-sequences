ef08f1203131543e8b3b647465c60442962ff4d2
Andrey Karamnoff <karamnoff@gmail.com>
2018-07-01 16:48:04 +0300

    add solution

--- src/html-tags.js
+++ src/html-tags.js
@@ -1,4 +1,4 @@
-/* eslint-disable import/prefer-default-export */
+/* eslint-disable import/prefer-default-export, arrow-body-style, consistent-return */
 import {
   isEmpty,
   head,
@@ -16,6 +16,29 @@ import {
 type Message = 'car' | 'cdr';
 type Pair = (message: Message) => any;
 type List = (...args: any) => any | null;
+
+export const reduce = (fn: Function, acc: number, elements: List) => {
+  const iter = (udList: List, accumulator: number) => {
+    if (isEmpty(udList)) {
+      return accumulator;
+    }
+    const item: Pair = head(udList);
+    const newAcc: number = (fn(item, acc)) ? accumulator + 1 : accumulator;
+    return iter(tail(udList), newAcc);
+  };
+  return iter(elements, 0);
+};
+
+export const emptyTagsCount = (tagName: string, html: List) => {
+  return reduce((element: Pair, acc: number) => {
+    if (is(tagName, element)) {
+      if (value(element)) {
+        return acc;
+      }
+      return acc + 1;
+    }
+  }, 0, html);
+};
 // END
 
 export const headersCount = (tagName: string, elements: List) => {
@@ -23,7 +46,6 @@ export const headersCount = (tagName: string, elements: List) => {
     if (isEmpty(items)) {
       return acc;
     }
-
     const item: Pair = head(items);
     const newAcc: number = is(tagName, item) ? acc + 1 : acc;
     return iter(tail(items), newAcc);
