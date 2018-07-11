e66bcc475fc131c68b347fb68516ba35704f9762
Andrey Karamnoff <karamnoff@gmail.com>
2018-07-11 20:28:51 +0300

    add solution

--- src/zip.js
+++ src/zip.js
@@ -3,6 +3,15 @@ import { l, isEmpty, head, tail, cons, reverse, toString as listToString } from 
 
 // BEGIN (write your solution here)
 // @flow
+type List = (...args: any) => any | null;
 
+const zip = (list1: List, list2: List): List => {
+  if (isEmpty(list1) || isEmpty(list2)) {
+    return l();
+  }
+  return cons(l(head(list1), head(list2)), zip(tail(list1), tail(list2)));
+};
+
+export default zip;
 // END
 
