85dc8a9c14270ad1e6f62b5b390298ac0872bbf7
Andrey Karamnoff <karamnoff@gmail.com>
2018-07-23 23:02:57 +0300

    add solution

--- src/solution.js
+++ src/solution.js
@@ -1,9 +1,27 @@
 /* eslint-disable import/prefer-default-export */
 // eslint-disable-next-line
-import { l, isEmpty, head, tail, filter, toString as listToString } from 'hexlet-pairs-data';
+import {
+  l,
+  isEmpty,
+  head,
+  tail,
+  filter,
+  toString as listToString
+} from 'hexlet-pairs-data';
 
 // BEGIN (write your solution here)
 // @flow
+type List = (...args: any) => any | null;
 
+export default (list: List): List => {
+  if (isEmpty(list)) {
+    return l();
+  }
+  return filter((element: number): List => {
+    if (head(list) % 2 === 0) {
+      return element % 2 === 0;
+    }
+    return element % 2 !== 0;
+  }, list);
+};
 // END
-
