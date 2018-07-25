1d89eefb08f49edf9b57adc0d6fcac435bcd37d2
Andrey Karamnoff <karamnoff@gmail.com>
2018-07-25 14:50:31 +0300

    add solution

--- src/take.js
+++ src/take.js
@@ -4,5 +4,15 @@ import { l, isEmpty, head, tail, cons, toString as listToString } from 'hexlet-p
 
 // BEGIN (write your solution here)
 // @flow
+type List = (...args: any) => any | null;
 
+const take = (n: number, list: List): List => {
+  if (isEmpty(list) || n === 0) {
+    return l();
+  }
+
+  return cons(head(list), take(n - 1, tail(list)));
+};
+
+export default take;
 // END
