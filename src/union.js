0d135f3fccef1544a946c4da80e63f96c32bf88f
Andrey Karamnoff <karamnoff@gmail.com>
2018-07-25 19:30:16 +0300

    add hexlet' solution

--- src/union.js
+++ src/union.js
@@ -1,9 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 // eslint-disable-next-line
 import {
-  head,
-  tail,
-  concat,
   l,
   isEmpty,
   cons,
@@ -17,55 +14,14 @@ import {
 // @flow
 type List = (...args: any) => any | null;
 
-// const has = (list: List, element: mixed): boolean => {
-//   if (isEmpty(list)) {
-//     return false;
-//   }
-//   if (head(list) === element) {
-//     return true;
-//   }
-
-//   return has(tail(list), element);
-// };
-
-// const concat = (list1: List, list2: List): List => {
-//   if (isEmpty(list1)) {
-//     return list2;
-//   }
-
-//   return cons(head(list1), concat(tail(list1), list2));
-// };
-
-// iterative process
-// const unique = (list: List): List => {
-//   const iter = (items: List, acc: List): List => {
-//     if (isEmpty(items)) {
-//       return reverse(acc);
-//     }
-
-//     const current = head(items);
-//     const rest = tail(items);
-//     const newAcc = has(rest, current) ? acc : cons(current, acc);
-
-//     return iter(rest, newAcc);
-//   };
-//   return iter(list, l());
-// };
-
-const unique = (list: List): List => {
-  const func = (element: mixed, acc: List): List => {
-    if (has(acc, element)) {
-      return acc;
-    }
-    return cons(element, acc);
-  };
-  return reverse(reduce(func, l(), list));
+const appendUniq = (base: List, list: List): List => {
+  const result = reduce(
+    (value: mixed, acc: List) => (has(acc, value) ? acc : cons(value, acc)),
+    reverse(base),
+    list,
+  );
+  return reverse(result);
 };
 
-export default (list1: List, list2: List): List => {
-  if (isEmpty(list1) && isEmpty(list2)) {
-    return l();
-  }
-  return unique(concat(unique(list1), unique(list2)));
-};
+export default (list1, list2) => appendUniq(appendUniq(l(), list1), list2);
 // END
