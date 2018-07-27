f0240d867fe696cc1ec4787be3658e88cd0fca83
Andrey Karamnoff <karamnoff@gmail.com>
2018-07-28 00:19:26 +0300

    add solution

--- src/sort.js
+++ src/sort.js
@@ -1,9 +1,37 @@
 /* eslint-disable import/prefer-default-export */
 // eslint-disable-next-line
-import { l, isEmpty, head, tail, cons, concat, filter, toString as listToString } from 'hexlet-pairs-data';
+import {
+  l,
+  isEmpty,
+  head,
+  tail,
+  cons,
+  concat,
+  filter,
+  toString as listToString,
+} from 'hexlet-pairs-data';
 
 // BEGIN (write your solution here)
 // @flow
+type List = (...args: any) => any | null;
 
+const sort = (list: List): List => {
+  if (isEmpty(list)) {
+    return l();
+  }
+
+  const base: number = head(list);
+  const firstMember: List = filter(
+    (element: number): boolean => element < base,
+    tail(list),
+  );
+  const secondMember: List = filter(
+    (element: number): boolean => element >= base,
+    tail(list),
+  );
+
+  return concat(sort(firstMember), cons(base, sort(secondMember)));
+};
+
+export default sort;
 // END
-
