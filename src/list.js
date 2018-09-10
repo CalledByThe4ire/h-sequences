480485bbbac1e38766219cc41a546322c5199484
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-10 22:21:55 +0300

    add 'toString' func for list-challenge

--- src/list.js
+++ src/list.js
@@ -7,6 +7,22 @@ export const l = (...items) => items.join(delimiter);
 
 // BEGIN (write your solution here)
 // @flow
+export const toString = (list) => {
+  const iter = (acc, str) => {
+    const fragment =
+      indexOf(str, delimiter) !== -1
+        ? substr(str, 0, indexOf(str, delimiter))
+        : substr(str);
+    const idx = length(fragment) + 1;
 
+    if (length(str) === 0) {
+      return `(${substr(acc, 0, length(acc) - 1)})`;
+    }
+    return iter(
+      `${acc}${fragment}${length(str) - length(fragment) === 0 ? '' : ','} `,
+      substr(str, idx),
+    );
+  };
+  return iter('', list);
+};
 // END
-
