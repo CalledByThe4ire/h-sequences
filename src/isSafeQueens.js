b8907d15f1c7c7e172034020269f7ddeeb845514
Andrey Karamnoff <karamnoff@gmail.com>
2018-09-12 13:53:16 +0300

    add solution for queens-challenge

--- src/isSafeQueens.js
+++ src/isSafeQueens.js
@@ -4,5 +4,38 @@ import { l, isEmpty, reverse, toString as listToString, isList, head, tail, cons
 
 // BEGIN (write your solution here)
 // @flow
+type List = (...args: mixed) => mixed | null;
+
+const isUnique = (list: List): boolean => {
+  if (isEmpty(tail(list))) {
+    return true;
+  }
+
+  const current: mixed = head(list);
+  const rest: List = tail(list);
+  const predicate: () => boolean = (element: mixed) => current !== element;
+  const func = (element: mixed): boolean => predicate(element);
+  const reduced: boolean = reduce(func, true, rest);
+
+  if (reduced) {
+    return isUnique(rest);
+  }
+  return false;
+};
+
+const isPairDiffHigherThanOne = (list: List): boolean => {
+  if (isEmpty(tail(list))) {
+    return true;
+  }
+
+  const first: mixed = head(list);
+  const second: mixed = head(tail(list));
+  if (Math.abs(first - second) <= 1) {
+    return false;
+  }
+  return isPairDiffHigherThanOne(tail(list));
+};
+
+export default (list: List): boolean => isUnique(list) && isPairDiffHigherThanOne(list);
 
 // END
