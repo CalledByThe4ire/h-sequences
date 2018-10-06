10a9a4ab719d0a07d880034c1536e8b25801c271
Andrey Karamnoff <karamnoff@gmail.com>
2018-10-06 08:40:21 +0300

    add solution for 'hierarchy-challenge'

--- src/solution.js
+++ src/solution.js
@@ -1,7 +1,71 @@
 /* eslint-disable import/prefer-default-export */
-import { l, isEmpty, head, tail, concat, toString as listToString } from 'hexlet-pairs-data'; // eslint-disable-line
-import { is, toString as htmlToString, hasChildren, children, reduce } from 'hexlet-html-tags'; // eslint-disable-line
+import {
+  l,
+  isEmpty,
+  head,
+  tail,
+  concat,
+  toString as listToString,
+} from 'hexlet-pairs-data'; // eslint-disable-line
+import {
+  is,
+  toString as htmlToString,
+  hasChildren,
+  children,
+  reduce,
+} from 'hexlet-html-tags'; // eslint-disable-line
 
 // BEGIN (write your solution here)
+const selectByTags = (query, root) => {
+  const iter = (tags, tree, acc) => {
+    if (isEmpty(tree)) {
+      return acc;
+    }
 
+    // tags
+    const tag = head(tags);
+    const restTags = tail(tags);
+
+    // nodes
+    const element = head(tree);
+    const restNodes = tail(tree);
+
+    if (isEmpty(restTags) && is(tag, element)) {
+      return tree;
+    }
+
+    if (hasChildren(element) && is(tag, element)) {
+      return iter(restTags, children(element), acc);
+    }
+    return iter(tags, restNodes, acc);
+  };
+  return iter(query, root, l());
+};
+
+const select = (query, root) => {
+  const iter = (tags, tree, acc) => {
+    if (isEmpty(tree)) {
+      return acc;
+    }
+
+    // nodes
+    const element = head(tree);
+    const restNodes = tail(tree);
+
+    if (hasChildren(element)) {
+      return iter(
+        tags,
+        restNodes,
+        iter(
+          tags,
+          children(element),
+          concat(acc, selectByTags(tags, l(element))),
+        ),
+      );
+    }
+    return iter(tags, restNodes, concat(acc, selectByTags(tags, l(element))));
+  };
+  return iter(query, root, l());
+};
+export default select;
 // END
