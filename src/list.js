3e6f674b1705bc0cb1fd6713e5883a231dc714f7
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-03 14:00:43 +0300

    add hexlet' solution for list-challenge

--- src/list.js
+++ src/list.js
@@ -1,93 +1,55 @@
-/* eslint-disable import/prefer-default-export */
-import { length, substr, indexOf } from './strings';
-
-const delimiter: string = '\n';
-export const l = (...items: Array<any>): string => items.join(delimiter);
+const delimiter = '\n';
+export const l = (...items) => items.join(delimiter);
 
 // BEGIN (write your solution here)
-// @flow
-export const toString = (list: string): string => {
-  const iter = (acc: string, str: string) => {
-    const fragment: string =
-      indexOf(str, delimiter) !== -1
-        ? substr(str, 0, indexOf(str, delimiter))
-        : substr(str);
-    const idx: number = length(fragment) + 1;
-
-    if (length(str) === 0) {
-      return `(${substr(acc, 0, length(acc) - 1)})`;
-    }
-    return iter(
-      `${acc}${fragment}${length(str) - length(fragment) === 0 ? '' : ','} `,
-      substr(str, idx),
-    );
-  };
-  return iter('', list);
+export const head = (list) => {
+  const index = list.indexOf(delimiter);
+  return list.substr(0, index > -1 ? index : list.length);
 };
 
-export const head = (list: string): string => {
-  if (indexOf(list, delimiter) === -1) {
-    return list;
-  }
-  return substr(list, 0, indexOf(list, delimiter));
+export const tail = (list) => {
+  const index = list.indexOf(delimiter);
+  return index > -1 ? list.substr(index + delimiter.length) : l();
 };
 
-export const tail = (list: string): string => {
-  if (indexOf(list, delimiter) === -1) {
-    return '';
-  }
-  return substr(list, indexOf(list, delimiter) + 1);
-};
+export const isEmpty = list => list.length === 0;
 
-export const isEmpty = (list: string): boolean => length(list) === 0;
+export const cons = (item, list) =>
+  (isEmpty(list) ? item : `${item}${delimiter}${list}`);
 
-export const cons = (item: any, list: string): string => {
-  if (length(list) === 0) {
-    return item;
-  }
-  return `${item}\n${list}`;
-};
-
-const reverse = (list) => {
-  const iter = (acc, items) => {
-    if (isEmpty(items)) {
+export const reduce = (func, init, list) => {
+  const iter = (rest, acc) => {
+    if (isEmpty(rest)) {
       return acc;
     }
-    return iter(cons(head(items), acc), tail(items));
+    return iter(tail(rest), func(head(rest), acc));
   };
-  return iter(l(), list);
+  return iter(list, init);
 };
 
-export const filter = (predicate: () => boolean, list) => {
-  const iter = (acc, items) => {
-    if (isEmpty(items)) {
-      return reverse(acc);
-    }
+export const reverse = list =>
+  reduce((item, acc) => cons(item, acc), l(), list);
 
-    const item = head(items);
-    const newAcc = predicate(item) ? cons(item, acc) : acc;
-    return iter(newAcc, tail(items));
-  };
-  return iter(l(), list);
+export const filter = (predicate, list) => {
+  const reversedResult = reduce(
+    (item, acc) => (predicate(item) ? cons(item, acc) : acc),
+    l(),
+    list,
+  );
+  return reverse(reversedResult);
 };
 
-export const map = (callback: Function, list) => {
-  const iter = (acc, items) => {
-    if (isEmpty(items)) {
-      return reverse(acc);
-    }
-    return iter(cons(callback(head(items)), acc), tail(items));
-  };
-
-  return iter(l(), list);
+export const map = (func, list) => {
+  const reversedResult = reduce(
+    (item, acc) => cons(func(item), acc),
+    l(),
+    list,
+  );
+  return reverse(reversedResult);
 };
 
-export const reduce = (callback: Function, init, list) => {
-  if (isEmpty(list)) {
-    return init;
-  }
-
-  return reduce(callback, callback(head(list), init), tail(list));
+export const toString = (list) => {
+  const str = reduce((item, acc) => `${acc}, ${item}`, head(list), tail(list));
+  return `(${str})`;
 };
-
 // END
