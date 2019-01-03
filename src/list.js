741d2b0db08af81bbd87b5d76cd03f84b0d9bfa0
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-03 14:28:01 +0300

    add hexlet' solution for list-challenge

--- src/list.js
+++ src/list.js
@@ -1,24 +1,29 @@
-const delimiter = '\n';
-export const l = (...items) => items.join(delimiter);
+// @flow
+const delimiter: string = '\n';
+export const l = (...items: Array<any>): string => items.join(delimiter);
 
 // BEGIN (write your solution here)
-export const head = (list) => {
+export const head = (list: string): string => {
   const index = list.indexOf(delimiter);
   return list.substr(0, index > -1 ? index : list.length);
 };
 
-export const tail = (list) => {
-  const index = list.indexOf(delimiter);
+export const tail = (list: string): string => {
+  const index: number = list.indexOf(delimiter);
   return index > -1 ? list.substr(index + delimiter.length) : l();
 };
 
-export const isEmpty = list => list.length === 0;
+export const isEmpty = (list: string): boolean => list.length === 0;
 
-export const cons = (item, list) =>
+export const cons = (item: string, list: string): string =>
   (isEmpty(list) ? item : `${item}${delimiter}${list}`);
 
-export const reduce = (func, init, list) => {
-  const iter = (rest, acc) => {
+export const reduce = (
+  func: (item: string, acc: string) => string,
+  init: string,
+  list: string,
+): string => {
+  const iter = (rest: string, acc: string): string => {
     if (isEmpty(rest)) {
       return acc;
     }
@@ -27,10 +32,10 @@ export const reduce = (func, init, list) => {
   return iter(list, init);
 };
 
-export const reverse = list =>
+export const reverse = (list: string): string =>
   reduce((item, acc) => cons(item, acc), l(), list);
 
-export const filter = (predicate, list) => {
+export const filter = (predicate: (item: string) => boolean, list: string): string => {
   const reversedResult = reduce(
     (item, acc) => (predicate(item) ? cons(item, acc) : acc),
     l(),
@@ -39,7 +44,7 @@ export const filter = (predicate, list) => {
   return reverse(reversedResult);
 };
 
-export const map = (func, list) => {
+export const map = (func: (item: string) => string, list: string): string => {
   const reversedResult = reduce(
     (item, acc) => cons(func(item), acc),
     l(),
@@ -48,7 +53,7 @@ export const map = (func, list) => {
   return reverse(reversedResult);
 };
 
-export const toString = (list) => {
+export const toString = (list: string): string => {
   const str = reduce((item, acc) => `${acc}, ${item}`, head(list), tail(list));
   return `(${str})`;
 };
