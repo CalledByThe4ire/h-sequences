8c0730c74de13d047c75e43ec55bbec174c6747e
Andrey Karamnoff <karamnoff@gmail.com>
2019-01-04 10:33:01 +0300

    add hexlet' solution for queens-challenge

--- src/isSafeQueens.js
+++ src/isSafeQueens.js
@@ -15,88 +15,20 @@ import {
 } from 'hexlet-pairs-data'; // eslint-disable-line
 
 // BEGIN (write your solution here)
-type List = (...args: Array<number>) => number | null;
+// @flow
+type List = (...args: any) => any | null;
 
-const isUnique = (list: List): boolean => {
-  const iter = (items: List) => {
-    const rest: List = tail(items);
-    if (isEmpty(rest)) {
-      return true;
-    }
-    const current: number = head(items);
-    const reduced: number = reduce(
-      (item, num) => (item === current ? num - 1 : num),
-      1,
-      list,
-    );
-    if (!reduced) {
-      return iter(rest);
-    }
-    return false;
-  };
-  return iter(list);
+const isSafeQueens = (list: List) => {
+  if (isEmpty(list)) {
+    return true;
+  }
+  const y1: number = head(list);
+  const checkSafe: boolean = reduce(
+    (y2: number, x: number) => (!x || y1 === y2 || Math.abs(y1 - y2) === x ? false : x + 1),
+    1,
+    tail(list),
+  );
+  return checkSafe ? isSafeQueens(tail(list)) : false;
 };
-
-const makePairDiffList = (list: List) => {
-  const iter = (items: List, acc: List) => {
-    const rest: List = tail(items);
-
-    if (isEmpty(rest)) {
-      return acc;
-    }
-
-    const first: number = head(items);
-    const second: number = head(rest);
-    const diff: number = Math.abs(first - second);
-    const newAcc: List = cons(diff, acc);
-
-    return iter(rest, newAcc);
-  };
-  return iter(list, l());
-};
-
-const isSiblingPairDiffRepeat = (list: List) => {
-  const diffList: List = makePairDiffList(list);
-  const iter = (items: List) => {
-    const rest: List = tail(items);
-
-    if (isEmpty(rest)) {
-      return true;
-    }
-
-    const first: number = head(items);
-    const second: number = head(rest);
-
-    if (first === second) {
-      return false;
-    }
-
-    return iter(rest);
-  };
-  return iter(diffList);
-};
-
-const isPairDiffHigherThanOne = (list: List) => {
-  const diffList: List = makePairDiffList(list);
-  const iter = (items: List) => {
-    if (isEmpty(items)) {
-      return true;
-    }
-
-    const current: number = head(items);
-    const rest: List = tail(items);
-
-    if (current > 1) {
-      return iter(rest);
-    }
-
-    return false;
-  };
-  return iter(diffList);
-};
-
-export default (list: List): boolean =>
-  isUnique(list) &&
-  isPairDiffHigherThanOne(list) &&
-  isSiblingPairDiffRepeat(list);
+export default isSafeQueens;
 // END
